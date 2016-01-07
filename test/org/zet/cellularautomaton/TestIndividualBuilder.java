8f65ba54b398a2b1864fcf809932be6d587c6f0e
Jan-Philipp Kappmeier <jp.kappmeier@gmail.com>
2016-01-08 00:51:33 +0100

    test case for resetting

--- test/org/zet/cellularautomaton/TestIndividualBuilder.java
+++ test/org/zet/cellularautomaton/TestIndividualBuilder.java
@@ -23,17 +23,18 @@ public class TestIndividualBuilder {
         assertThat(i1, is(not(same(i2))));
         assertThat(i1, is(not(equalTo(i2))));
     }
+
+    final int age = 43;
+    final double exhaustionFactor = 0.1;
+    final double familiarity = 0.2;
+    final double panicFactor = 0.3;
+    final double reactionTime = 14;
+    final double relativeMaxSpeed = 3.1;
+    final double slackness = 0.8;
     
     @Test
     public void propertiesStored() {
         IndividualBuilder b = new IndividualBuilder();
-        final int age = 43;
-        final double exhaustionFactor = 0.1;
-        final double familiarity = 0.2;
-        final double panicFactor = 0.3;
-        final double reactionTime = 14;
-        final double relativeMaxSpeed = 3.1;
-        final double slackness = 0.8;
         
         Individual i = b.withAge(age).withExhaustionFactor(exhaustionFactor).withFamiliarity(familiarity)
                 .withPanicFactor(panicFactor).withReactionTime(reactionTime).withRelativeMaxSpeed(relativeMaxSpeed)
@@ -48,12 +49,30 @@ public class TestIndividualBuilder {
         assertThat(i.getSlackness(), is(closeTo(slackness, 10e-6)));
 
         Individual i2 = b.build();
+        assertEqualProperties(i, i2);
+    }
+    
+    @Test
+    public void resetting() {
+        IndividualBuilder b = new IndividualBuilder();
+        
+        Individual defaultIndividual = b.build();
+        
+        b.withAge(age).withExhaustionFactor(exhaustionFactor).withFamiliarity(familiarity)
+                .withPanicFactor(panicFactor).withReactionTime(reactionTime).withRelativeMaxSpeed(relativeMaxSpeed)
+                .withSlackness(slackness).buildAndReset();
+        
+        Individual resettedIndividual = b.build();
+        assertEqualProperties(defaultIndividual, resettedIndividual);
+    }
+    
+    private void assertEqualProperties(Individual i, Individual i2) {
         assertThat(i2.getAge(), is(equalTo(i.getAge())));
         assertThat(i2.getExhaustionFactor(), is(equalTo(i.getExhaustionFactor())));
         assertThat(i2.getFamiliarity(), is(equalTo(i.getFamiliarity())));
         assertThat(i2.getPanicFactor(), is(equalTo(i.getPanicFactor())));
         assertThat(i2.getReactionTime(), is(equalTo(i.getReactionTime())));
         assertThat(i2.getRelativeSpeed(), is(equalTo(i.getRelativeSpeed())));
-        assertThat(i2.getSlackness(), is(equalTo(i.getSlackness())));
+        assertThat(i2.getSlackness(), is(equalTo(i.getSlackness())));        
     }
 }
