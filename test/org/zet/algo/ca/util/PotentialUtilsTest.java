5b86ad312c5eb0977a6bfb4fd50a7759d5b3856f
Jan-Philipp Kappmeier <jp.kappmeier@gmail.com>
2016-07-27 23:14:36 +0200

    additional tests for potential utils

--- test/org/zet/algo/ca/util/PotentialUtilsTest.java
+++ test/org/zet/algo/ca/util/PotentialUtilsTest.java
@@ -1,10 +1,12 @@
 package org.zet.algo.ca.util;
 
 
-import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
-import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.collection.IsIterableContainingInAnyOrder.containsInAnyOrder;
+import static org.jmock.AbstractExpectations.returnValue;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
@@ -14,6 +16,7 @@ import org.junit.Test;
 import org.zet.cellularautomaton.EvacCell;
 import org.zet.cellularautomaton.EvacCellInterface;
 import org.zet.cellularautomaton.EvacuationCellState;
+import org.zet.cellularautomaton.ExitCell;
 import org.zet.cellularautomaton.potential.StaticPotential;
 
 /**
@@ -78,5 +81,77 @@ public class PotentialUtilsTest {
             }
         };        
     }
+    
+    @Test
+    public void negativeIsIgnored() {
+        StaticPotential sp1 = new StaticPotential();
+        StaticPotential sp2 = new StaticPotential();
 
+        EvacCellInterface cell1 = context.mock(EvacCellInterface.class, "cell1");
+        EvacCellInterface cell2 = context.mock(EvacCellInterface.class, "cell2");
+
+        List<StaticPotential> potentials = new LinkedList<>();
+
+        sp1.setPotential(cell1, 1);
+        sp1.setPotential(cell2, -1);
+        potentials.add(sp1);
+
+        sp2.setPotential(cell1, -1);
+        sp2.setPotential(cell2, 2);
+        potentials.add(sp2);
+
+        // Expectations
+        context.checking(new Expectations() {
+            {
+                allowing(cell1).getX();
+                allowing(cell2).getX();
+                allowing(cell1).getY();
+                will(returnValue(1));
+                allowing(cell2).getY();
+                will(returnValue(2));
+            }
+        });
+
+        StaticPotential minimum = PotentialUtils.mergePotentials(potentials);
+
+        assertThat(minimum.getPotential(cell1), is(equalTo(1)));
+        assertThat(minimum.getPotential(cell2), is(equalTo(2)));
+    }
+
+    
+    @Test
+    public void associatedExitCellsMerged() {
+        StaticPotential sp1 = new StaticPotential();
+        StaticPotential sp2 = new StaticPotential();
+
+        ExitCell cell1 = new ExitCell(0, 1);
+        ExitCell cell2 = new ExitCell(0, 2);
+        EvacCellInterface cell3 = context.mock(EvacCellInterface.class, "standard cell");
+
+        List<StaticPotential> potentials = new LinkedList<>();
+
+        sp1.setPotential(cell1, 1);
+        sp1.setPotential(cell2, 2);
+        sp1.setPotential(cell3, 3);
+        sp1.getAssociatedExitCells().add(cell1);
+        potentials.add(sp1);
+
+        sp2.setPotential(cell1, -1);
+        sp2.setPotential(cell2, 2);
+        sp2.setPotential(cell3, 2);
+        sp2.getAssociatedExitCells().add(cell2);
+        potentials.add(sp2);
+
+        // Expectations
+        context.checking(new Expectations() {
+            {
+                allowing(cell3).getX();
+                allowing(cell3).getY();
+            }
+        });
+
+        StaticPotential minimum = PotentialUtils.mergePotentials(potentials);
+
+        assertThat(minimum.getAssociatedExitCells(), containsInAnyOrder(new ExitCell[] {cell1, cell2}));
+    }
 }
