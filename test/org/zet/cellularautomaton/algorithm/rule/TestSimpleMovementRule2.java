e0a28e169e91027805936e0828280dc39ddfa2c4
Jan-Philipp Kappmeier <jp.kappmeier@gmail.com>
2017-01-20 23:10:46 +0100

    simplified tests

--- test/org/zet/cellularautomaton/algorithm/rule/TestSimpleMovementRule2.java
+++ test/org/zet/cellularautomaton/algorithm/rule/TestSimpleMovementRule2.java
@@ -4,17 +4,18 @@ import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.closeTo;
+import static org.jmock.AbstractExpectations.any;
+import static org.jmock.AbstractExpectations.returnValue;
 import static org.jmock.AbstractExpectations.same;
 import static org.zet.cellularautomaton.algorithm.rule.RuleTestMatchers.executeableOn;
 
 import java.util.List;
 import java.util.Collections;
 import java.util.function.Function;
-import static org.hamcrest.Matchers.closeTo;
-import static org.jmock.AbstractExpectations.any;
-import static org.jmock.AbstractExpectations.returnValue;
 import org.jmock.Expectations;
 import org.jmock.Mockery;
+import org.junit.Before;
 import org.junit.Test;
 import org.zet.cellularautomaton.EvacCell;
 import org.zet.cellularautomaton.EvacCellInterface;
@@ -27,6 +28,7 @@ import org.zet.cellularautomaton.algorithm.state.EvacuationState;
 import org.zet.cellularautomaton.algorithm.state.EvacuationStateControllerInterface;
 import org.zet.cellularautomaton.algorithm.state.IndividualProperty;
 import org.zet.cellularautomaton.statistic.CAStatisticWriter;
+import org.zetool.common.util.Direction8;
 
 /**
  *
@@ -38,9 +40,15 @@ public class TestSimpleMovementRule2 {
 
         private final EvacCell targetCell;
         int counter = 0;
+        private final Direction8 direction;
 
         public FakeSimpleMovementRule2(EvacCell targetCell) {
+            this(targetCell, null);
+        }
+
+        public FakeSimpleMovementRule2(EvacCell targetCell, Direction8 direction8) {
             this.targetCell = targetCell;
+            this.direction = direction8;
         }
 
         @Override
@@ -59,53 +67,38 @@ public class TestSimpleMovementRule2 {
             return true;
         }
 
+        @Override
+        protected Direction8 getDirection() {
+            return direction != null ? direction : super.getDirection();
+        }
     };
 
-    @Test
-    public void executeableIfNotEmpty() {
-        SimpleMovementRule rule = new SimpleMovementRule();
-        EvacCell cell = new RoomCell(0, 0);
-        assertThat(rule, is(not(executeableOn(cell))));
-        Individual i = new Individual(0, 0, 0, 0, 0, 0, 1, 0);
-        cell.getState().setIndividual(i);
-        assertThat(rule, is(executeableOn(cell)));
-    }
+    private final int TIME_STEP = 13;
+    private final int STEP_END_TIME = 5;
 
-    @Test
-    public void noMoveIfNotAlarmed() {
-        Mockery context = new Mockery();
+    private Mockery context = new Mockery();
+    private EvacCell testCell;
+    private Individual i = new Individual(0, 0, 0, 0, 0, 0, 1, 0);
+    private Room room;
+    private EvacuationState es;
+    private EvacuationStateControllerInterface ec;
+    private IndividualProperty ip;
 
-        Room room = context.mock(Room.class);
+    @Before
+    public void initState() {
+        room = context.mock(Room.class);
+        es = context.mock(EvacuationState.class);
+        ec = context.mock(EvacuationStateControllerInterface.class);
+        ip = new IndividualProperty(i);
 
         context.checking(new Expectations() {
             {
                 allowing(room).getID();
                 will(returnValue(1));
-            }
-        });
-        EvacCell testCell = new RoomCell(1, 0, 0, room);
-
-        Individual i = new Individual(0, 0, 0, 0, 0, 0, 1, 0);
-        testCell.getState().setIndividual(i);
-        FakeSimpleMovementRule2 rule = new FakeSimpleMovementRule2(testCell) {
-
-            @Override
-            public void move(EvacCellInterface from, EvacCellInterface targetCell) {
-                throw new AssertionError("Move should not be called!");
-            }
-        };
-
-        EvacuationState es = context.mock(EvacuationState.class);
-        EvacuationStateControllerInterface ec = context.mock(EvacuationStateControllerInterface.class);
-        IndividualProperty ip = new IndividualProperty(i);
-        ip.setCell(testCell);
-        ip.setStepEndTime(0);
-
-        context.checking(new Expectations() {
-            {
                 allowing(es).propertyFor(i);
                 will(returnValue(ip));
-                allowing(ec).move(testCell, testCell);
+                allowing(es).getTimeStep();
+                will(returnValue(TIME_STEP));
                 allowing(room).getID();
                 will(returnValue(1));
                 allowing(es).getStatisticWriter();
@@ -116,6 +109,45 @@ public class TestSimpleMovementRule2 {
                 will(returnValue(false));
             }
         });
+        testCell = new RoomCell(1, 0, 0, room);
+        testCell.getState().setIndividual(i);
+        ip.setCell(testCell);
+        ip.setStepEndTime(STEP_END_TIME);
+
+    }
+
+    @Test
+    public void executeableIfNotEmpty() {
+        SimpleMovementRule2 rule = new SimpleMovementRule2();
+
+        // Not excuteable when empty
+        assertThat(rule, is(not(executeableOn(new RoomCell(0, 0)))));
+
+        // Executeable when filled
+        assertThat(rule, is(executeableOn(testCell)));
+    }
+
+    @Test
+    public void noMoveIfNotAlarmed() {
+
+        FakeSimpleMovementRule2 rule = new FakeSimpleMovementRule2(testCell) {
+
+            @Override
+            public void move(EvacCellInterface from, EvacCellInterface targetCell) {
+                throw new AssertionError("Move should not be called!");
+            }
+        };
+
+        context.checking(new Expectations() {
+            {
+                allowing(ec).move(testCell, testCell);
+
+                // No cells
+                allowing(room).existsCellAt(with(any(Integer.class)), with(any(Integer.class)));
+                will(returnValue(false));
+            }
+        });
+
         assertThat(ip.isAlarmed(), is(false));
 
         rule.setEvacuationState(es);
@@ -125,8 +157,8 @@ public class TestSimpleMovementRule2 {
         assertThat(rule.counter, is(equalTo(0)));
 
         // Assert that one time step is used for not doing anything
-        assertThat(ip.getStepStartTime(), is(closeTo(0.0, 0.0001)));
-        assertThat(ip.getStepEndTime(), is(closeTo(1.0, 0.0001)));
+        assertThat(ip.getStepStartTime(), is(closeTo(STEP_END_TIME, 0.0001)));
+        assertThat(ip.getStepEndTime(), is(closeTo(STEP_END_TIME + 1, 0.0001)));
     }
 
     /**
@@ -134,20 +166,6 @@ public class TestSimpleMovementRule2 {
      */
     @Test
     public void noMoveWhenLastMoveOngoing() {
-        Mockery context = new Mockery();
-
-        Room room = context.mock(Room.class);
-
-        context.checking(new Expectations() {
-            {
-                allowing(room).getID();
-                will(returnValue(1));
-            }
-        });
-        EvacCell testCell = new RoomCell(1, 0, 0, room);
-
-        Individual i = new Individual(0, 0, 0, 0, 0, 0, 1, 0);
-        testCell.getState().setIndividual(i);
         FakeSimpleMovementRule2 rule = new FakeSimpleMovementRule2(testCell) {
 
             @Override
@@ -156,28 +174,18 @@ public class TestSimpleMovementRule2 {
             }
         };
 
-        EvacuationState es = context.mock(EvacuationState.class);
-        EvacuationStateControllerInterface ec = context.mock(EvacuationStateControllerInterface.class);
-        IndividualProperty ip = new IndividualProperty(i);
         ip.setCell(testCell);
-        final double stepStartTime = 12.2;
-        final double stepEndTime = 13.8;
+        final double stepStartTime = TIME_STEP - 0.8;
+        final double stepEndTime = TIME_STEP + 0.8;
+
         ip.setStepEndTime(stepEndTime);
         ip.setStepStartTime(stepStartTime);
         ip.setAlarmed();
 
         context.checking(new Expectations() {
             {
-                allowing(es).propertyFor(i);
-                will(returnValue(ip));
                 never(ec).move(with(any(EvacCell.class)), with(any(EvacCell.class)));
-                allowing(room).getID();
-                will(returnValue(1));
-                allowing(es).getStatisticWriter();
-                will(returnValue(new CAStatisticWriter(es)));
-                allowing(es).getTimeStep();
-                will(returnValue(13));
-                
+
                 allowing(room).existsCellAt(with(any(Integer.class)), with(any(Integer.class)));
                 will(returnValue(false));
             }
@@ -193,23 +201,20 @@ public class TestSimpleMovementRule2 {
         assertThat(Double.doubleToRawLongBits(stepStartTime), is(equalTo(Double.doubleToRawLongBits(ip.getStepStartTime()))));
         assertThat(Double.doubleToRawLongBits(ip.getStepEndTime()), is(equalTo(Double.doubleToRawLongBits(stepEndTime))));
     }
-    
+
     @Test
     public void moveCallsCellularAutomaton() {
-        Mockery context = new Mockery();
-        EvacuationStateControllerInterface ec = context.mock(EvacuationStateControllerInterface.class);
-        EvacCell startCell = new RoomCell(0, 0);
         EvacCell targetCell = new RoomCell(0, 0);
 
         context.checking(new Expectations() {
             {
-                exactly(1).of(ec).move(with(startCell), with(targetCell));
+                exactly(1).of(ec).move(with(testCell), with(targetCell));
             }
         });
         SimpleMovementRule rule = new SimpleMovementRule();
 
         rule.setEvacuationStateController(ec);
-        rule.move(startCell, targetCell);
+        rule.move(testCell, targetCell);
 
         context.assertIsSatisfied();
     }
@@ -218,39 +223,38 @@ public class TestSimpleMovementRule2 {
     public void sameCellSelectedIfEmptyTargetList() {
         List<EvacCellInterface> targets = Collections.emptyList();
         SimpleMovementRule rule = new SimpleMovementRule();
-        EvacCell currentCell = new RoomCell(0, 0);
 
-        EvacCellInterface selectedCell = rule.selectTargetCell(currentCell, targets);
+        EvacCellInterface selectedCell = rule.selectTargetCell(testCell, targets);
 
-        assertThat(selectedCell, is(same(currentCell)));
+        assertThat(selectedCell, is(same(testCell)));
     }
 
     @Test
     public void singleCellSelected() {
-        SimpleMovementRule rule = new SimpleMovementRule();
-        EvacCellInterface currentCell = new RoomCell(0, 0);
-        Individual i = new Individual(0, 0, 0, 0, 0, 0, 1, 0);
-        currentCell.getState().setIndividual(i);
-        EvacCell targetCell = new RoomCell(0, 0);
-        List<EvacCellInterface> targets = Collections.singletonList(targetCell);
+        SimpleMovementRule2 rule = new SimpleMovementRule2() {
+            @Override
+            protected void noMove() {
+                // Nothing
+            }
+        };
 
-        Mockery context = new Mockery();
-        EvacuationState es = context.mock(EvacuationState.class);
+        List<EvacCellInterface> targets = Collections.singletonList(testCell);
+
         Computation c = context.mock(Computation.class);
         EvacuationCellularAutomatonInterface ca = context.mock(EvacuationCellularAutomatonInterface.class);
         context.checking(new Expectations() {
             {
-                allowing(es).getCellularAutomaton();
-                will(returnValue(ca));
-                allowing(c).effectivePotential(with(i), with(targetCell), with(any(Function.class)));
+                allowing(c).effectivePotential(with(i), with(testCell), with(any(Function.class)));
                 will(returnValue(1.0));
             }
         });
         rule.setEvacuationState(es);
         rule.setComputation(c);
+        ip.setAlarmed();
 
-        EvacCellInterface selectedCell = rule.selectTargetCell(currentCell, targets);
+        rule.execute(testCell);
+        EvacCellInterface selectedCell = rule.selectTargetCell(testCell, targets);
 
-        assertThat(selectedCell, is(same(targetCell)));
+        assertThat(selectedCell, is(same(testCell)));
     }
 }
